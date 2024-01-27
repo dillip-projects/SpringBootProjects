36d6c715548952f05425a8be35fa3058393d3e3b
dillip-projects <104688042+dillip-projects@users.noreply.github.com>
2024-01-28 01:36:30 +0530

    Delete src directory

--- src/main/java/com/example/EmployeManagement/repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.EmployeManagement.repository;
-
-import com.example.EmployeManagement.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-}
